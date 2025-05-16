665a2a6b1108d546e6866ce534095bbb5f83817a
PHIL HÄGELI <phil.haegeli8@gmail.com>
2025-05-16 13:27:50 +0200

    Added Test

--- src/tanzverbot-diet.test.ts
+++ src/tanzverbot-diet.test.ts
@@ -3,3 +3,7 @@ import { calcDateOnDiet, Sex } from "./tanzverbot-diet";
 test("Tanzverbot Diet", () => {
   expect(calcDateOnDiet(74, 100, 1.86, 38, Sex.Male)).toBeGreaterThan(0);
 });
+
+test("Pommespanzer 150KG", () => {
+  expect(calcDateOnDiet(79, 150, 1.81, 17, Sex.Male)).toBeCloseTo(99);
+});
